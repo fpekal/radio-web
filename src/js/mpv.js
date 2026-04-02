f2ff4a76a4ba33b55778f450b78281006a87b535
Filip Pękala <272985@student.pwr.edu.pl>
2026-04-02 19:12:52 +0200

    Use mpv-ipc and change how mpv server is run

--- src/js/mpv.js
+++ src/js/mpv.js
@@ -1,22 +1,35 @@
-import { exec } from 'child_process';
+import { spawn } from 'child_process';
+import { MPVClient } from 'mpv-ipc'
 
-class Player {
-  constructor() {
-	let runtime_dir = process.env.XDG_RUNTIME_DIR
-
-	exec('mpv --no-video --idle --input-ipc-server=' + runtime_dir + '/radioweb-mpv.socket &',
-	  function callback(error, stdout, stderr) {
-		console.log('stdout: ' + stdout)
-	  })
-
-	console.log('MPV started')
-  }
-}
+let player
+let player_promise_resolve
+let player_promise = new Promise((resolve) => {player_promise_resolve = resolve})
 
 function init() {
-  const player = new Player()
+  let runtime_dir = process.env.XDG_RUNTIME_DIR
+
+  spawn('mpv', ['--no-video', '--idle', '--input-ipc-server=' + runtime_dir + '/radioweb-mpv.socket'],
+	{
+	  detached: true,
+	  stdio: "ignore"
+	}).unref()
+
+  setTimeout(() => {
+	player = new MPVClient(runtime_dir + '/radioweb-mpv.socket')
+	player.on('event', (name, args) => {
+	  console.log(name, args)
+	})
+
+	player_promise_resolve()
+  }, 1000)
 
   console.log('MPV initialized')
+}
+
+export async function play(url) {
+  await player_promise
+
+  player.loadfile(url)
 }
 
 init()
